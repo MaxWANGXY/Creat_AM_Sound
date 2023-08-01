a7eeb1c6113419cb926f91bcd658c87e26f0190f
MaxWANGXY <119681999+MaxWANGXY@users.noreply.github.com>
2023-08-01 17:13:13 -0400

    Update m2_37Hz_ASSR_Comparison.m

--- m2_37Hz_ASSR_Comparison.m
+++ m2_37Hz_ASSR_Comparison.m
@@ -3,11 +3,34 @@ clear
 close all
 tic
 %% 1 调频声音刺激
+
+flag_filter = 1
+
 load AM_Epoch.mat
 % 选择单通道或者多通道
-Chan_AVG_data = squeeze(mean(data([1:2 ],:,:),1)) ;
+Chan_AVG_data = squeeze(mean(data([1:2],:,:),1)) ;
 % 选择平均的试次数量
 Wav4PSD = squeeze(mean(Chan_AVG_data(TimeIndex>=150 & TimeIndex<=450,:),2))' ;
+
+%% 滤波器设置
+if flag_filter
+    % 设定滤波器频率边界
+    lowcut = 30;
+    highcut = 50;
+
+    % 设定滤波器阶数
+    order = 6; % 这是一个常用的值，但你可以根据需要调整
+
+    % 创建滤波器
+    [b, a] = butter(order, [lowcut highcut]/(Fs/2), 'bandpass');
+
+    % 应用滤波器
+    filtered_signal = filtfilt(b, a, double(Wav4PSD));
+
+    clear Wav4PSD
+    Wav4PSD = filtered_signal ;
+end
+
 
 % 零填充以达到1Hz的频谱分辨率
 N = 5000; % 零填充后的样本数量
@@ -30,16 +53,34 @@ set(gca,'fontsize',30)
 xlabel('Frequency (Hz)');
 ylabel('Power ');
 title('AM-Epoch');
-xlim([30 44]);
+xlim([25 50]);
 
 %% 2 非调频声音刺激
-clear
+clearvars -except flag_filter
 
 load No_AM_Epoch.mat
 % 选择单通道或者多通道
 Chan_AVG_data = squeeze(mean(data([1:2 ],:,:),1)) ;
 % 选择平均的试次数量
 Wav4PSD = squeeze(mean(Chan_AVG_data(TimeIndex>=150 & TimeIndex<=350,:),2))' ;
+
+if flag_filter
+    % 设定滤波器频率边界
+    lowcut = 30;
+    highcut = 50;
+
+    % 设定滤波器阶数
+    order = 6; % 这是一个常用的值，但你可以根据需要调整
+
+    % 创建滤波器
+    [b, a] = butter(order, [lowcut highcut]/(Fs/2), 'bandpass');
+
+    % 应用滤波器
+    filtered_signal = filtfilt(b, a, double(Wav4PSD));
+
+    clear Wav4PSD
+    Wav4PSD = filtered_signal ;
+end
 
 % 零填充以达到1Hz的频谱分辨率
 N = 5000; % 零填充后的样本数量
